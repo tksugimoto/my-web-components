bee18b636913ecf8336930aed565e8b6947c341b
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-01-02 21:13:14 +0900

    [Update] check-box: class構文での定義に変更

--- check-box/check-box.js
+++ check-box/check-box.js
@@ -6,50 +6,50 @@
 	
 	const template = thisDoc.querySelector("template").content;
 	
-	const MyElementProto = window.Object.create(window.HTMLElement.prototype);
-		
-	window.Object.defineProperty(MyElementProto, "checked", {
-		set: function (value) {
+	class CheckBoxElement extends HTMLElement {
+		constructor() {
+			super();
+		}
+
+		set checked(value) {
 			this.checkbox.checked = !!value;
 			this.updateCheckedAttribute();
-		},
-		get: function () {
+		}
+		get checked() {
 			return this.checkbox.checked;
 		}
-	});
-	
-	MyElementProto.updateCheckedAttribute = function () {
-		if (this.checkbox.checked) {
-			this.setAttribute("checked", true);
-		} else {
-			this.removeAttribute("checked");
-		}
-	};
 
-	MyElementProto.createdCallback = function () {
-		this.createShadowRoot();
-		
-		const clone = thatDoc.importNode(template, true);
-		this.shadowRoot.appendChild(clone);
-		
-		this.checkbox = this.shadowRoot.querySelector("input");
-		
-		if (this.hasAttribute("checked")) {
-			this.checkbox.checked = true;
+		updateCheckedAttribute() {
+			if (this.checkbox.checked) {
+				this.setAttribute("checked", true);
+			} else {
+				this.removeAttribute("checked");
+			}
 		}
-		if (this.hasAttribute("disabled")) {
-			this.checkbox.disabled = true;
+
+		createdCallback() {
+			this.createShadowRoot();
+			
+			const clone = thatDoc.importNode(template, true);
+			this.shadowRoot.appendChild(clone);
+			
+			this.checkbox = this.shadowRoot.querySelector("input");
+			
+			if (this.hasAttribute("checked")) {
+				this.checkbox.checked = true;
+			}
+			if (this.hasAttribute("disabled")) {
+				this.checkbox.disabled = true;
+			}
+			
+			this.checkbox.addEventListener("change", () => {
+				this.updateCheckedAttribute();
+				const event = new window.Event("change");
+				event.checked = this.checkbox.checked;
+				this.dispatchEvent(event);
+			});
 		}
-		
-		this.checkbox.addEventListener("change", () => {
-			this.updateCheckedAttribute();
-			const event = new window.Event("change");
-			event.checked = this.checkbox.checked;
-			this.dispatchEvent(event);
-		});
-	};
+	}
 	
-	thatDoc.registerElement("check-box", {
-		prototype: MyElementProto
-	});
+	thatDoc.registerElement("check-box", CheckBoxElement);
 })(window, document);
